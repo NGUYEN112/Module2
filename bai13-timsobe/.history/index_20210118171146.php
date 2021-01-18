1628d82fe0aa2244f9a2ec80020f3a2bfab972c8
NGUYEN112 <phannguyenhoainguyen@gmail.com>
2021-01-18 19:39:24 +0700

    tim so be nhat trong mang

---
+++ bai13-timsobe/.history/index_20210118171146.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tìm số nhỏ nhất sd phương thức</title>
+</head>
+<body>
+    <?php
+    $array = [6,3,7,93,73,4,7,2];
+    $min = $array[0];
+    $index = 0;
+    function minarr($array,$min,$index) {
+        for ($i = 1; $i<count($array) ; $i++ ) {
+            if ($array[$i] < $min) {
+                $min = $array[$i];
+                $index = $i;
+                echo $i;
+            }
+        }
+        return;
+    }
+    minarr($array,$min,$index);
+    // echo $min. " " .$index;
+    ?>
+</body>
+</html>
