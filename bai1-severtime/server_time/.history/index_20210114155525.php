8db71dcaae33df25da7b0e89ff96a09f6262ce1e
NGUYEN112 <phannguyenhoainguyen@gmail.com>
2021-01-18 19:34:27 +0700

    ung dung hien thi thoi gian cua sever

---
+++ bai1-severtime/server_time/.history/index_20210114155525.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thoi gian hien tai</title>
+</head>
+<body>
+    <h1>Bay gio la: <?php echo date_default_timezone_set('Asia/Ho_Chi_Minh') ?>
+    </h1>
+</body>
+</html>
